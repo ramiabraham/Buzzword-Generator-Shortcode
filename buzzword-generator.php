8a22d1676f7b62fc466591eb082e6e07f7c18b98
Rami Abraham <r@radesign.ws>
2013-08-05 20:30:11 -0400

    Update buzzword-generator.php

--- buzzword-generator.php
+++ buzzword-generator.php
@@ -6,6 +6,8 @@ Description: Makes marketing bullshit based on the old dack.com arrays
 Version: 1.0
 Author: ramiabraham
 Contributors: ramiabraham
+License: GPLv2 or later
+Tested up to: 3.6
 Author URI: http://rami.nu
 */
 
